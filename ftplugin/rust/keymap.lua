b5127ea276715f0be557ae9b47e294f74eb8f5fa
harryhanYuhao <s2162783@ed.ac.uk>
2023-04-26 16:37:32 +0100

    formatted keymap update

--- ftplugin/rust/keymap.lua
+++ ftplugin/rust/keymap.lua
@@ -1,3 +1,3 @@
 vim.keymap.set('i', '<F3>',  "<Esc>:w<CR>:!rustc <C-R>% -o a.out <CR>", {noremap = true})
 vim.keymap.set('n', '<F3>', ':!./a.out<CR>', {noremap = true})
-vim.keymap.set('n', '<leader>rf',  "<Esc>:w<CR>:!rustfmt <C-R>%  <CR>", {noremap = true})
+vim.keymap.set('n', '<leader>fo',  "<Esc>:w<CR>:!rustfmt <C-R>%  <CR>", {noremap = true})
