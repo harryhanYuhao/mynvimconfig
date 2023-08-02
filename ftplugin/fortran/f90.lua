af2c8eb986be6226d6d9328dd609b9852e5c24cc
Harry Han (Yuhao) <harryhan912@gmail.com>
2023-08-02 22:22:12 +0800

    fortran support update

--- ftplugin/fortran/f90.lua
+++ ftplugin/fortran/f90.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('i', '<F3>', '<Esc>:w<CR>:!gfortran <C-R>%<CR>', {noremap = true}) -- -w to hide warnings
+vim.keymap.set('i', '<F3>', '<Esc>:w<CR>:!gfortran <C-R>% -pedantic -Wall -Wextra <CR>', {noremap = true}) -- -w to hide warnings
 vim.keymap.set('n', '<F3>', ':!./a.out<CR>', {noremap = true})
