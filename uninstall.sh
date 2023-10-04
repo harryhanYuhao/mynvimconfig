b8829ccafa28d1a9719f35c03c19d1b4285b800a
Harry Han (Yuhao) <harryhan912@gmail.com>
2023-10-04 15:34:03 +0100

    bug fix: posix compliant

--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,7 @@ echo "Uninstalling (y/n)"
 echo "This script will attempt to remove irrevocaly all nvim files, configurations, and plugins systemwise"
 read answer
 
-if [[ "$answer" == "y" ]] && [[ `id -u` == 0 ]]; then
+if [ "$answer" = "y" ] && [ `id -u` = 0 ]; then
     echo "Removing nvim files"
     rm -rf ~/.config/nvim
     rm -rf ~/.local/share/nvim
